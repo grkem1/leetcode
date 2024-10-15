58e00ea20c78dce1829a9542a275dfc00fe09fa1
Görkem Kılınç <grkemkilinc@gmail.com>
2024-10-15 22:41:07 +0200

    Medium problem separate black/white
    Calculate required number of operations during bubble sort

---
+++ src/separate-black-and-white-balls/Solution.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/separate-black-and-white-balls
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        steps = 0
+        zeros = 0
+        for i in range(len(s)-1, -1, -1):
+            if(s[i] == "0"):
+                zeros += 1
+            else:
+                steps += zeros
+        return steps
