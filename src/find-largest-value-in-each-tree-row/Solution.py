5eb4ccbf962cfbb2512187e367b6e62013155b0f
Görkem Kılınç <grkemkilinc@gmail.com>
2024-12-25 15:59:22 +0100

    Another approach to a previously solved problem
    Find largest values of rows in a binary tree

--- src/find-largest-value-in-each-tree-row/Solution.py
+++ src/find-largest-value-in-each-tree-row/Solution.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/find-largest-value-in-each-tree-row
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,22 +9,44 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if(root == None):
+        if root == None:
             return []
         q = deque()
         q.append(root)
         q.append(None)
         largests = []
-        largest = -2**31
-        while(len(q) > 1):
+        largest = -(2**31)
+        while len(q) > 1:
             node = q.popleft()
-            if(node == None):
+            if node == None:
                 largests.append(largest)
-                largest = -2**31
+                largest = -(2**31)
                 q.append(None)
                 continue
-            largest = max(largest,node.val)
-            if(node.left):q.append(node.left)
-            if(node.right):q.append(node.right)
+            largest = max(largest, node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
         largests.append(largest)
         return largests
+
+
+# 25.12.2024 edit: dfs solution... works faster but uses more memory, as expected.
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def dfs(n, level=0):
+            nonlocal result
+            if not n:
+                return
+            if len(result) <= level:
+                result.append(n.val)
+            else:
+                result[level] = max(result[level], n.val)
+            dfs(n.left, level + 1)
+            dfs(n.right, level + 1)
+
+        dfs(root)
+        return result
